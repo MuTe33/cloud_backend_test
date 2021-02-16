6b4ccf2eadb39739e6035e6f5c7a7ffaac5d0739
MuTe33 <48680353+MuTe33@users.noreply.github.com>
2021-02-16 15:57:55 +0100

    New setup.

--- lib/backend/functions/src/index.ts
+++ lib/backend/functions/src/index.ts
@@ -1,9 +1,34 @@
-import * as functions from "firebase-functions";
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import * as express from 'express';
+import * as bodyParser from "body-parser";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
+//initialize firebase inorder to access its services
+admin.initializeApp(functions.config().firebase);
+
+//initialize express server
+const app = express();
+const main = express();
+
+//add the path to receive request and set json as bodyParser to process the body
+main.use('/', app);
+main.use(bodyParser.json());
+main.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/hello', async (req, res) => {
+  try {
+    res.status(200).json('World!');
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+//define google cloud function name
+exports = module.exports = functions.https.onRequest(main);
+
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
 //
 // export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
+//  response.send("Hello from Firebase!");
 // });
